6982ecd058b815388ef84c2ab76e8916bed010c8
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-24 03:31:56 +0100

    warpwallet version v0.1.2

--- Formula/warpwallet.rb
+++ Formula/warpwallet.rb
@@ -1,9 +1,9 @@
 class Warpwallet < Formula
   desc "A deterministic bitcoin address generator"
   homepage "http://github.com/moncho/warpwallet"
-  url "https://github.com/moncho/warpwallet/releases/download/v0.1.1/warpwallet_0.1.1_darwin_amd64.tar.gz"
-  version "0.1.1"
-  sha256 "25e5e628a56562ebceeeaece3c80f7d191189fd46bedcb7bcffe02c6addc9d5f"
+  url "https://github.com/moncho/warpwallet/releases/download/v0.1.2/warpwallet_0.1.2_darwin_amd64.tar.gz"
+  version "0.1.2"
+  sha256 "0884b85d6ab8e8a7d80118176795f0491be3f720d40db85a0d5af308a2b74886"
 
   def install
     bin.install "warpwallet"
